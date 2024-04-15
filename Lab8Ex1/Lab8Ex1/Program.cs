e96c39a290c2b256f471abcb23bf6bd034ee4170
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-04-15 20:21:24 +0200

    Add files via upload

---
+++ Lab8Ex1/Lab8Ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Un tren este compus din: o locomotiva si mai multe tipuri de vagoane
+class Tren
+{
+    //Trenul va avea un nume precum si urmatoarele metode
+    private string nume;
+    public Tren (string nume)
+    {
+        this.nume = nume;
+    }
+    //Adauga vagon: Aceasta metoda va permite adaugarea vagoanelor la tren
+    public void AdaugaVagon()
+    {
+        Console.WriteLine($"se pot adauga vagoane");
+    }
+    //Pleaca din gara: La aceasta comanda, trenul va inchide usile vagoanelor de persoane si va porni locomotiva
+    public void PleacaDinGara()
+    {
+        Console.WriteLine($"trenul va pleca din gara");
+    }
+    // Opreste in gara: La aceasta comanda, trenul va opri locomotiva,
+    // va deschide usile vagoanelor de persoane si va opri aerul conditionat in vagoanele clasa 1 
+    public void OpresteInGara()
+    {
+        Console.WriteLine($"trenul va orpi in gara");
+    }
+}
+class Locomotiva: Tren 
+{
+    // Locomotiva va avea
+    //O metoda de pornire, la apelul careia,
+    //aceasta va confirma faptul ca a pornit printr-un mesaj afisat in consola, 
+    public void Pornire()
+    {
+        Console.WriteLine($"locomotiva a pornit");
+    }
+    //O metoda de oprire, la apelul careia,
+    //aceasta va confirma faptul ca a oprit printr-un mesaj afisat in consola,
+    public void Oprire()
+    {
+        Console.WriteLine($"Locomotiva s-a oprit");
+    }
+}
---
+++ Lab8Ex1/Lab8Ex1/Vagoane.cs
@@ -0,0 +1,67 @@
+﻿using Lab8Ex1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lab8Ex1
+{
+    //Vagoanele vor avea masa si anul fabricatiei si vor fi de mai multe tipuri
+    internal class Vagoane
+    {
+        private int masa;
+        private int andefabricatie;
+        public Vagoane(int masa, int andefabricatie)
+        {
+            this.masa = masa;
+            this.andefabricatie = andefabricatie;
+        }
+    }
+}
+//Vagoane de marfa, care vor avea tipul marfii (cereale/carbuni/otel) precum si capacitatea in tone
+class VagoaneDeMarfa: Vagoane
+{
+    private string tipulmarfii;
+    private int capacitate;
+    public Vagoanemarfa(string tipulmarfii, int capacitate)
+    {
+        this.tipulmarfii = tipulmarfii;
+        this.capacitate = capacitate;
+    }
+}
+//Vagoane pentru persoane, care vor avea numar de locuri
+class VagoaneDePersoane: Vagoane
+{
+    private int numardelocuri;
+    public VagoaneDePersoane(int numardelocuri)
+    {
+        this.numardelocuri = numardelocuri;
+    }
+    //O metoda care va inchide usile si va confirma in consola inchiderea usilor
+    public void InchideUsile()
+    {
+        Console.WriteLine($"se vor inchide usile");
+    }
+    //O metoda care va deschide usile si va confirma in consola deschiderea usilor
+    public void DeschideUsile()
+    {
+        Console.WriteLine($"se vor deschide usile");
+    }
+}
+//Vagoane clasa I, pentru persoane
+
+class VagoaneClasaI : Vagoane
+{
+    //O metoda de pornire a aerului conditionat care va afisa acest lucru pe ecran
+    public void AerConditioant()
+    {
+        Console.WriteLine($"Se porneste aerul conditionat");
+    }
+    //O metoda de oprire a aerului conditionat care va afisa acest lucru pe ecran
+    public void OprireAC()
+    {
+        Console.WriteLine($"SE opreste aerul codnitionat");
+    }
+}
